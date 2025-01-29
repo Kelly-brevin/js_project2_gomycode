779263a402d672e931d40469efac2260f4e1b142
kelly brevin <kellybrevin83@gmail.com>
2025-01-29 21:09:07 +0300

    new-commit

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+// MATHEMATICAL FUNCTIONS
+
 // Prime Number Check: Create a function to check if a number is prime or not.
-// function checkPrime() {
-//   let userinput = prompt(" Enter a year to check whether it is leap or not");
-//   if (userinput <= 0 || isNaN(userinput)) {
-//     prompt("please enter a valid year");
-//   } else if (
-//     userinput % 4 == 0 ||
-//     userinput % 100 == 1 ||
-//     userinput % 400 == 0
-//   ) {
-//     alert(`The year ${userinput} is a leap year`);
-//   } else {
-//     alert(`The year ${userinput} is not a leap year`);
-//   }
-// }
+
+function checkPrime() {
+  let userinput = prompt(" Enter a year to check whether it is leap or not");
+  if (userinput <= 0 || isNaN(userinput)) {
+    prompt("please enter a valid year");
+  } else if (
+    userinput % 4 == 0 ||
+    userinput % 100 == 1 ||
+    userinput % 400 == 0
+  ) {
+    alert(`The year ${userinput} is a leap year`);
+  } else {
+    alert(`The year ${userinput} is not a leap year`);
+  }
+}
 // checkPrime();
 
 //Factorial: Write a function to calculate the factorial of a given number.
@@ -30,4 +33,6 @@ function factorial() {
   }
   alert(`the factorial of ${number} is ${factorial}`);
 }
-factorial();
+// factorial();
+
+// Fibbonachi sequence.  Implement a function to generate the Fibonacci sequence up to a given number of terms
